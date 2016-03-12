00d53ca3af740bab9021d2e935872ee6a62b0175
Pierre-Antoine Champin <pchampin@liris.cnrs.fr>
2016-03-12 18:30:41 +0100

    added tests for issues #6 and #7

---
+++ utests/test_issue_6.py
@@ -0,0 +1,39 @@
+from fsa4streams.fsa import FSA
+from test_fsa import assert_matches
+from unittest import skip
+
+@skip('not fixed yet')
+def test_issue_6_a():
+    fsa = FSA.make_empty(allow_overlap=True)
+    (fsa
+     .add_state("start")
+       .add_transition("a", "s1")
+     .add_state("s1")
+       .add_transition("b", "s2")
+       .add_transition("b", "s3", silent=True)
+     .add_state("s2", max_noise=1)
+       .add_transition("c", "finish")
+     .add_state("s3", max_noise=1)
+       .add_transition("d", "finish")
+     .add_state("finish", terminal=True)
+     .check_structure()
+    )
+    assert_matches(fsa, "abcd", ["abc", "ad"])
+
+@skip('not fixed yet')
+def test_issue_6_b():
+    fsa = FSA.make_empty(allow_overlap=True)
+    (fsa
+     .add_state("start")
+       .add_transition("a", "s1")
+     .add_state("s1")
+       .add_transition("b", "s2", silent=True)
+       .add_transition("b", "s3")
+     .add_state("s2", max_noise=1)
+       .add_transition("c", "finish")
+     .add_state("s3", max_noise=1)
+       .add_transition("d", "finish")
+     .add_state("finish", terminal=True)
+     .check_structure()
+    )
+    assert_matches(fsa, "abcd", ["ac", "abd"])
---
+++ utests/test_issue_7.py
@@ -0,0 +1,26 @@
+from fsa4streams.fsa import FSA
+from test_fsa import assert_matches
+from unittest import skip
+
+@skip('not fixed yet')
+def test_issue_7():
+    fsa = FSA.from_dict({
+        "allow_overlap": True,
+        "states": {
+            "start": {
+                "default_transition": {
+                    "target": "s1"
+                }
+            },
+            "s1": {
+                "default_transition": {
+                    "target": "s2"
+                }
+            },
+            "s2": {
+                "terminal": True
+            }
+        }
+    })
+    assert_matches(fsa, "ab", ["ab"])
+    
